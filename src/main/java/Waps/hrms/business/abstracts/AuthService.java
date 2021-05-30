058289ee783065d144c2538797ef6e01062ef6fa
Your Name <you@example.com>
2021-05-30 21:57:22 +0300

    30 Mayıs 2021

--- src/main/java/Waps/hrms/business/abstracts/AuthService.java
+++ src/main/java/Waps/hrms/business/abstracts/AuthService.java
@@ -4,7 +4,7 @@ import Waps.hrms.core.utilities.results.Result;
 import Waps.hrms.entities.concretes.Candidate;
 import Waps.hrms.entities.concretes.Employer;
 
-public interface AuthService {
+public interface AuthService{
 
 	
 	Result registerCandidate(Candidate candidate);
